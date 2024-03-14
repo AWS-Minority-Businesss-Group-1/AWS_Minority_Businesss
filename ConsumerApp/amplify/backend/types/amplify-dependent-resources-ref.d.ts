e8dfcb7499a5c019002e75c17e91bed71f4d5036
ChristianASC5 <41081554+ChristianASC5@users.noreply.github.com>
2024-03-14 19:28:06 -0400

    Added backend resources

--- ConsumerApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ ConsumerApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "consumerapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "ConsumerApp": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
