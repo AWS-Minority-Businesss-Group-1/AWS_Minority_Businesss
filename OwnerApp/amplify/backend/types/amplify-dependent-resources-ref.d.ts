41d551449c216b0a4319ee9bb49ebb4377b7044d
ChristianASC5 <41081554+ChristianASC5@users.noreply.github.com>
2024-03-20 12:19:41 -0400

    Pushed backend changes to amplify

--- OwnerApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ OwnerApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,15 +6,19 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIKeyOutput": "string"
     }
   },
-  "auth": {
-    "ownerapp9e572191": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
+  "function": {
+    "S3Trigger8c51e57c": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "AvStorage": {
+      "BucketName": "string",
+      "Region": "string"
     }
   }
 }
