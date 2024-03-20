2ad8afdd9249c2832be19da2f59e252b06abbd44
ChristianASC5 <41081554+ChristianASC5@users.noreply.github.com>
2024-03-20 12:35:44 -0400

    Pushed file storage to amplify backend

--- OwnerApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ OwnerApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "OwnerFileStorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
