8c3c8fea4e8c02408fb1e2ef02f33abab4245e22
ChristianASC5 <41081554+ChristianASC5@users.noreply.github.com>
2024-03-20 13:48:43 -0400

    Revert "Pushed file storage to amplify backend"
    This reverts commit 2ad8afdd9249c2832be19da2f59e252b06abbd44.

--- OwnerApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ OwnerApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,11 +16,5 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
-  },
-  "storage": {
-    "OwnerFileStorage": {
-      "BucketName": "string",
-      "Region": "string"
-    }
   }
 }
