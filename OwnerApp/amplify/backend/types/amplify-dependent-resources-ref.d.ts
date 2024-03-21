00d9c3016b5147d5c6143128713ea5e7e511d5ae
ChristianASC5 <41081554+ChristianASC5@users.noreply.github.com>
2024-03-21 01:38:26 -0400

    Push auth backend to cloud

--- OwnerApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ OwnerApp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,5 +5,16 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
     }
+  },
+  "auth": {
+    "ownerapp70c2d3df": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
   }
 }
